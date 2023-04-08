bb4b7565157e799dd43415859d622c00f419a4a1
Mike <hgelhem@gmail.com>
2023-04-08 10:11:40 +0300

    добавил putRequest

--- src/main/java/app/RestTemplate/RestTemplateApplication.java
+++ src/main/java/app/RestTemplate/RestTemplateApplication.java
@@ -1,6 +1,7 @@
 package app.RestTemplate;
 
 import app.model.User;
+import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -8,10 +9,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 @SpringBootApplication
 public class RestTemplateApplication {
 
@@ -26,29 +34,37 @@ public class RestTemplateApplication {
 
 		String part1 = postRequest(headers, restTemplate);
 
+		String part2 = putRequest(headers, restTemplate);
+
 		LOGGER.info(part1);
+		LOGGER.info(part2);
 		applicationContext.close();
 	}
 
 	static HttpHeaders getUsers(RestTemplate restTemplate) {
 		ResponseEntity<User[]> response = restTemplate.getForEntity(URL, User[].class);
 		HttpHeaders headers = response.getHeaders();
-
-		var objectList = Arrays.asList(response.getBody());
+		User[] body = Objects.requireNonNull(response.getBody());
+		var objectList = Arrays.asList(body);
 		objectList.forEach(o -> LOGGER.info(o.toString()));
 
 		return headers;
 	}
 
+	@SneakyThrows
 	static String postRequest(HttpHeaders headers, RestTemplate restTemplate) {
 		var user = new User(3, "James", "Brown", 25);
 		String json = user.getJson();
-		HttpEntity<String> request = new HttpEntity<>(json, headers);
+		HttpEntity<String> postRequest = new HttpEntity<>(json, headers);
 
-		return restTemplate.postForObject(URL, request, String.class);
+		return restTemplate.postForObject(URL, postRequest, String.class);
 	}
 
-	static void putRequest(HttpHeaders headers, RestTemplate restTemplate) {
+	static String putRequest(HttpHeaders headers, RestTemplate restTemplate) {
+		var userUpdate = new User(3, "Thomas", "Shelby", 25);
+		String json = userUpdate.getJson();
+		HttpEntity<String> putRequest = new HttpEntity<>(json, headers);
 
+		return restTemplate.postForObject(URL, putRequest, String.class);
 	}
 }
